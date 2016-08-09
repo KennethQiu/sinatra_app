7065c3f3d7b1708beaecf0b965e9c36903e70b56
KennethQiu <kennethqiujunhao@gmail.com>
2016-08-09 00:25:27 +0000

    e2 added url button

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,7 @@ class Message < ActiveRecord::Base
 
   validates :content, presence: true
   validates :author, presence: true
+  validates :url, format: { with: /((?:https?\:\/\/|www\.)(?:[-a-z0-9]+\.)*[-a-z0-9]+.*)/, message: 'invalid url' } 
 
 
 end
